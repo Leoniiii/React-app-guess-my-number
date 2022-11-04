81a619df5c394b6df2fb2e0387e08e8f7ec26ab3
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2022-11-05 00:42:42 +0100

    Create custom buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,14 @@
+import { View, TextInput } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
 const StartGameScreen = () => {
-  return;
+  return (
+    <View>
+      <TextInput></TextInput>
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 };
 
 export default StartGameScreen;
